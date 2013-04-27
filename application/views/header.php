15657ca1f9fc74e7627ce866d032915615bd4222
Karim Mansour <senegalo@gmail.com>
2013-04-27 16:54:28 +0200

    Fixed scalling on small devices

--- application/views/header.php
+++ application/views/header.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title>INDEXIE</title>
+        <title>Indexie</title>
 
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="<? print base_url('public/css/bootstrap.min.css'); ?>" rel="stylesheet" type="text/css"/>
         <link href="<? print base_url('public/css/bootstrap-responsive.min.css'); ?>" rel="stylesheet" type="text/css"/>
         <link href="<? print base_url('public/css/main.css'); ?>" rel="stylesheet" type="text/css"/>
